32efa4541bf6fdce12b745d460232fd256e02c6f
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-07-12 10:19:08 -0400

    Create fs_snapshot.py

---
+++ fs_snapshot.py
@@ -0,0 +1,24 @@
+import os
+from hashlib import sha256
+snap1 = {}
+snap2 = {}
+for root, files, dirs in os.walk("/"):
+  for file in files:
+    snap1[os.path.join(root,file)] = sha256(open(os.path.join(root,file),"rb")).hexdigest()
+
+ print(snap1)
+
+cont = input("Press enter to continue: ")
+for root, files, dirs in os.walk("/"):
+  for file in files:
+    filename = os.path.join(root,file)
+    try:
+      oldsha = snap1[filename]
+    except:
+      print("File created: {}".format(filename))
+    else:
+      if sha256(open(filename,"rb")).hexdigest() == oldsha:
+        continue
+      else:
+        print("File {} changed: {} --> {}".format(filename,oldsha,sha256(open(filename,"rb")).hexdigest()))
+    
