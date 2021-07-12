e839555d7742078b3e458dc538c0fdaaa656a672
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-07-12 10:38:24 -0400

    Update fs_snapshot.py

--- fs_snapshot.py
+++ fs_snapshot.py
@@ -2,16 +2,26 @@ import os
 import hashlib
 snap1 = {}
 snap2 = {}
-for root, dirs, files in os.walk("/"):
+def arg(p):
+  try:
+    import sys
+    return sys.argv[p]
+  except:
+    return None
+  
+dirtowalk = "/"
+if arg(1) == "-d":
+  dirtowalk = arg(2)
+for root, dirs, files in os.walk(dirtowalk):
   for file in files:
     try:
       snap1[os.path.join(root,file)] = hashlib.sha256(open(os.path.join(root,file),"rb").read()).hexdigest()
     except Exception as err:
-      print(err)
+      pass
 print(snap1)
 
 cont = input("Press enter to continue: ")
-for root, dirs, files in os.walk("/"):
+for root, dirs, files in os.walk(dirtowalk):
   for file in files:
     filename = os.path.join(root,file)
     try:
