28ac47f505f378880082118f92fe03928b878d34
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-07-12 10:32:07 -0400

    Bug fix - read the file

--- fs_snapshot.py
+++ fs_snapshot.py
@@ -5,7 +5,7 @@ snap2 = {}
 for root, dirs, files in os.walk("/"):
   for file in files:
     try:
-      snap1[os.path.join(root,file)] = hashlib.sha256(open(os.path.join(root,file),"r").encode()).hexdigest()
+      snap1[os.path.join(root,file)] = hashlib.sha256(open(os.path.join(root,file),"rb").read()).hexdigest()
     except Exception as err:
       print(err)
 print(snap1)
@@ -19,8 +19,8 @@ for root, dirs, files in os.walk("/"):
     except:
       print("File created: {}".format(filename))
     else:
-      if hashlib.sha256(open(filename,"r").encode()).hexdigest() == oldsha:
+      if hashlib.sha256(open(filename,"rb").read()).hexdigest() == oldsha:
         continue
       else:
-        print("File {} changed: {} --> {}".format(filename,oldsha,hashlib.sha256(open(filename,"rb")).hexdigest()))
+        print("File {} changed: {} --> {}".format(filename,oldsha,hashlib.sha256(open(filename,"rb").read()).hexdigest()))
     
