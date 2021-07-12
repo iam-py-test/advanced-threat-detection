e68b2673238616530e8a54a367c0775012c2885d
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-07-12 10:23:36 -0400

    Update fs_snapshot.py

--- fs_snapshot.py
+++ fs_snapshot.py
@@ -4,7 +4,10 @@ snap1 = {}
 snap2 = {}
 for root, dirs, files in os.walk("/"):
   for file in files:
-    snap1[os.path.join(root,file)] = sha256(open(os.path.join(root,file),"rb")).hexdigest()
+    try:
+      snap1[os.path.join(root,file)] = sha256(open(os.path.join(root,file),"r").encode()).hexdigest()
+    except:
+      pass
 print(snap1)
 
 cont = input("Press enter to continue: ")
@@ -16,7 +19,7 @@ for root, dirs, files in os.walk("/"):
     except:
       print("File created: {}".format(filename))
     else:
-      if sha256(open(filename,"rb")).hexdigest() == oldsha:
+      if sha256(open(filename,"r").encode()).hexdigest() == oldsha:
         continue
       else:
         print("File {} changed: {} --> {}".format(filename,oldsha,sha256(open(filename,"rb")).hexdigest()))
